08b58c35c3d21113436400343d0d31d914bb4547
naaz289 <153366522+naaz289@users.noreply.github.com>
2024-08-31 23:11:20 +0530

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-images img');
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Optional: Auto-play functionality
+setInterval(nextSlide, 3000);
